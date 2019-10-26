fbd62eae8310a5906841816c716a5499cb0c076f
Serenade092 <serenade092@berkeley.edu>
2019-10-26 13:08:49 -0700

    Updates

---
+++ precache-manifest.27d5adf57edb46b6620c345d55f53044.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26d00a70d74885fd0bc89c9edc805a3",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "329d5cb26a69cf9701e0",
+    "url": "/repository-name/static/css/main.ddd6d20d.chunk.css"
+  },
+  {
+    "revision": "150c69342a9f68c5ebdf",
+    "url": "/repository-name/static/js/2.64c52e0e.chunk.js"
+  },
+  {
+    "revision": "329d5cb26a69cf9701e0",
+    "url": "/repository-name/static/js/main.f4d3a547.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "a2e673d28272146123d21631b44f4925",
+    "url": "/repository-name/static/media/CS61A.a2e673d2.png"
+  },
+  {
+    "revision": "7ba6d10e7d6bd7c4db4b9c279eb6d2fd",
+    "url": "/repository-name/static/media/Debugging-squirrel.7ba6d10e.png"
+  },
+  {
+    "revision": "02c69303ee28a8048d45cf80cea0d2ad",
+    "url": "/repository-name/static/media/MA.02c69303.png"
+  },
+  {
+    "revision": "bdf8bb1a51c07d97354fa8cbdc2ff2d2",
+    "url": "/repository-name/static/media/email.bdf8bb1a.png"
+  },
+  {
+    "revision": "b08b6e5278ea25ab488dec3d1b6bbb97",
+    "url": "/repository-name/static/media/github-logo.b08b6e52.png"
+  },
+  {
+    "revision": "00d32095bf094820dc947fc0c0be391c",
+    "url": "/repository-name/static/media/input.00d32095.png"
+  },
+  {
+    "revision": "4338745d85c07cf6b38d277e7f39bbfa",
+    "url": "/repository-name/static/media/interface.4338745d.png"
+  },
+  {
+    "revision": "ad2e08a183856cf5c8208584d0ec5e84",
+    "url": "/repository-name/static/media/linkedin-logo.ad2e08a1.png"
+  },
+  {
+    "revision": "e19cbe5c6d547d06400bffbaf2c0b05c",
+    "url": "/repository-name/static/media/me.e19cbe5c.jpg"
+  },
+  {
+    "revision": "a2f9e060f982377caeabc260bc3c502a",
+    "url": "/repository-name/static/media/observation.a2f9e060.png"
+  },
+  {
+    "revision": "3ef8b446d3de3b5986a2be76b55f4cb3",
+    "url": "/repository-name/static/media/squirrel_singing.3ef8b446.png"
+  }
+]);
